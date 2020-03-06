8188a2749545a8aa62403b330972f4ad8ee4d24f
Kevin <kevarifin14@gmail.com>
2020-03-05 19:10:58 -0600

    interest form with student/investor group breakdown

--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -5,11 +5,15 @@ import { rhythm } from '../utils/typography';
 
 function InterestForm() {
   const [email, setEmail] = useState('');
+  const [isStudent, setIsStudent] = useState(true);
   const [isEmailSubmitted, setIsEmailSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addToMailchimp(email)
+
+    const group = isStudent ? { 'group[26469][4]': '4' } : { 'group[26469][1]': '1' };
+
+    addToMailchimp(email, group)
       .then(() => {
         setEmail('');
         setIsEmailSubmitted(true);
@@ -19,9 +23,30 @@ function InterestForm() {
   return (
     <div class='card'>
       <div class='card-content'>
-        <h2 class='title'>Let's stay connected</h2>
+        <h2 class='title'>
+          {isStudent ?
+            'Get started with finding a mentor to pay for school' :
+            'Learn how you can partner with students as a mentor'}
+        </h2>
 
         <form onSubmit={handleSubmit}>
+
+          <div class="buttons has-addons is-centered">
+            <button
+              class={`button is-outline ${isStudent ? 'is-primary' : ''}`}
+              onClick={() => setIsStudent(true)}
+              type='button'
+            >
+              Student
+            </button>
+            <button
+              class={`button is-outline ${!isStudent ? 'is-primary' : ''}`}
+              onClick={() => setIsStudent(false)}
+              type='button'
+            >
+              Mentor
+            </button>
+          </div>
 
           <input
             style={{ marginBottom: rhythm(1) }}
@@ -43,7 +68,7 @@ function InterestForm() {
             Learn more
           </button>
 
-          {isEmailSubmitted && <h3 class='subtitle'>Thank you!</h3>}
+          {isEmailSubmitted && <h3 class='subtitle'>Thank you! We'll be reaching out shortly</h3>}
         </form>
       </div>
     </div>
