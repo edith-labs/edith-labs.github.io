0a03711b666dded9103b8bd89c0046ef6c09cf11
Kevin <kevarifin14@gmail.com>
2020-03-07 22:19:05 -0600

    Updates

--- content/blog/intro-to-isa/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Intro to Income Share Agreements
-date: "2020-02-24"
-excerpt: "Income Share Agreements "
----
-
-Test
---
+++ styles-58d6c0720c97ae1a01a7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{Sqhu:function(n,o,w){},bN3s:function(n,o,w){}}]);
+//# sourceMappingURL=styles-58d6c0720c97ae1a01a7.js.map
